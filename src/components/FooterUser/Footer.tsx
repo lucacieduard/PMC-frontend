21485faf53f625f33d3d6116d1fe3ff352e15c1b
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-12-28 15:09:40 +0200

    user - footer added

--- src/components/FooterUser/Footer.tsx
+++ src/components/FooterUser/Footer.tsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.scss";
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer style={{ background: "#2e1f5e" }}>
+      <div className={`${styles.footer} wrapper`}>
+        <img
+          className={styles.logo}
+          alt="atletis logo"
+          src="logo_faraNume.png"
+        />
+
+        <a
+          href="https://eduardlucaci.ro/"
+          target="_blank"
+          className={styles.by}
+        >
+          &#169; Lucaci Eduard
+        </a>
+
+        <Link to={"/admin"} className={styles.admin}>
+          Administrare{" "}
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
