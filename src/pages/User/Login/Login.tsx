01997938c6fb39cee6c98312f65126ec0675874d
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2024-01-05 14:51:10 +0200

    login

--- src/pages/User/Login/Login.tsx
+++ src/pages/User/Login/Login.tsx
@@ -1,26 +1,33 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styles from "./Login.module.scss";
+import { AuthContext } from "../../../context/AuthContext";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "../../../utils/fetch/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [form, setForm] = useState({ email: "", parola: "" });
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      authContext.setAuth(data.user);
+      navigate("/competitii");
+      toast.success("Te-ai conectat cu succes!");
+    },
+    onError: (error) => {
+      console.log(error.message);
+      toast.error("Email sau parola incorecta!");
+    },
+  });
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
-    const response = await fetch(
-      `${import.meta.env.VITE_API_URL}/api/utilizatori/autentificare`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-        credentials: "include",
-      }
-    );
-    const data = await response.json();
-    console.log(response);
-    console.log(data);
+    loginMutation.mutate(form);
   };
   return (
     <div className={styles.container}>
--- src/utils/fetch/auth.ts
+++ src/utils/fetch/auth.ts
@@ -1,13 +1,35 @@
-export const signup = async (data )  =>{
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/utilizatori/creeaza-cont`,{
-     headers: {
-         "Content-Type": "application/json",
-       },
-         method:"POST",
-         body: JSON.stringify(data),
-         credentials: "include"
-     })
-     if(!response.ok) throw new Error()
-     console.log(response)
-     return response.json()
- }
+export const signup = async (data: {
+  nume: string,
+  prenume: string,
+  email: string,
+  telefon: string,
+  clubSportiv: string,
+  parola: string,
+  confirmParola: string
+}) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/utilizatori/creeaza-cont`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(data),
+    credentials: "include"
+  })
+  if (!response.ok) throw new Error()
+  console.log(response)
+  return response.json()
+}
+
+export const login = async (data: { email: string, parola: string }) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/utilizatori/autentificare`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(data),
+    credentials: "include"
+  })
+  if (!response.ok) throw new Error()
+  console.log(response)
+  return response.json()
+}
